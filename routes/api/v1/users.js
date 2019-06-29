1d7d248ea69b46534700a8d3c4e562839862675f
wipegup <wipegup@gmail.com>
2019-06-29 04:53:42 -0600

    Pull out API key creation

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var User = require('../../../models').User;
 var bcrypt = require('bcrypt');
 var defaultHeader = ["Content-Type", "application/json"]
+
+const createApiKey = function(){
+  return Math.random().toString(36).substring(2);
+}
+
 router.post('/', function (req, res, next){
   if ( req.body.password != req.body.password_confirmation){
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader(...defaultHeader);
     res.status(500).send({ "error": "passwords do not match" });
   } else {
-    let api_key = Math.random().toString(36).substring(2);
+    let api_key = createApiKey();
 
     bcrypt.hash(req.body.password, 5, function(err, hash){
 
@@ -23,8 +28,9 @@ router.post('/', function (req, res, next){
       })
       .catch( error => {
         res.setHeader(...defaultHeader);
-        res.status(500).send({error})
+        res.status(500).send({error});
       })
+
     })
 
   }
