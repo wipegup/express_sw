01645229118d768d7e1d63bfaddb628cf526a9ad
wipegup <wipegup@gmail.com>
2019-06-29 13:07:38 -0600

    Add new packages and routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/api/v1/users');
+var forecastRouter = require('./routes/api/v1/forecast');
 
 var app = express();
 
@@ -16,5 +17,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', userRouter);
-
+app.use('/api/v1/forecast', forecastRouter);
 module.exports = app;
