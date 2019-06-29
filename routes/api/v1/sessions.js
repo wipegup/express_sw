1f6326535554b038334641a432c15725b48f4d14
wipegup <wipegup@gmail.com>
2019-06-29 15:19:38 -0600

    Refactor login code

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -4,29 +4,27 @@ var User = require('../../../models').User;
 var bcrypt = require('bcrypt');
 var defaultHeader = ["Content-Type", "application/json"]
 
-router.post('/', function(req, res, next){
-  let requestEmail = req.body.email;
-  let requestPassword = req.body.password;
+var incorrectLogin = function(res){
+  res.setHeader(...defaultHeader);
+  res.status(401).send({'error':'email or password incorrect'});
+}
 
-  User.findOne({ where: {email: requestEmail}})
+router.post('/', function(req, res, next){
+
+  User.findOne({ where: {email: req.body.email}})
     .then( user => {
       if (user === null){
-        res.setHeader(...defaultHeader);
-        res.status(401).send({'error':'email or password incorrect'})
+        incorrectLogin(res);
       } else {
-        let userPasswordDigest = user.password_digest;
-
-        bcrypt.compare(requestPassword, userPasswordDigest, function(err, correctPW){
+        bcrypt.compare(req.body.password, user.password_digest, function(err, correctPW){
           if (correctPW) {
             res.setHeader(...defaultHeader);
             res.status(200).send({'api_key': user.api_key})
           } else {
-            res.setHeader(...defaultHeader);
-            res.status(401).send({'error':'email or password incorrect'})
+            incorrectLogin(res);
           }
         });
       }
-
     })
 
 })
