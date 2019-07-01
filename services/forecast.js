11c06228d69dc47a6949f0ee758fd9a9d7f344de
wipegup <wipegup@gmail.com>
2019-06-30 20:44:38 -0600

    Change forecast function to include geocoding

--- services/forecast.js
+++ services/forecast.js
@@ -1,14 +1,21 @@
 const weatherSerializer = require('../serializers/weather_serializer');
 const fetch = require('node-fetch');
+const geocode = require('./geocode');
 
-var getWeather = function(latLng){
-  let darkSkyURL = "https://api.darksky.net/forecast/" + process.env.DARKSKY_KEY + "/";
-  darkSkyURL = darkSkyURL + latLng.lat.toString() + "," + latLng.lng.toString();
+var getWeather = function(location){
 
-  return fetch(darkSkyURL)
-    .then(res => { return res.json();})
-    .then(res => {
-      return weatherSerializer(res);
+
+
+  return geocode(location)
+    .then(latLng => {
+      // console.log(latLng);
+      let darkSkyURL = "https://api.darksky.net/forecast/" + process.env.DARKSKY_KEY + "/";
+      darkSkyURL = darkSkyURL + latLng.lat.toString() + "," + latLng.lng.toString();
+      return fetch(darkSkyURL)
+      .then(res => { return res.json();})
+      .then(res => {
+        return weatherSerializer(res);
+      })
     })
 }
 
